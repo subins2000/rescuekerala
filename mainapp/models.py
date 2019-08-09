b4017fd95e77f82b6b29a92faee283f00ba8555e
Abhiram R <abhi.darkness@gmail.com>
2019-08-10 03:40:43 +0530

    Added help-text to hashtag field to aid entry: #1017

--- mainapp/models.py
+++ mainapp/models.py
@@ -550,7 +550,7 @@ class Announcements(models.Model):
         default='L')
 
     description = models.TextField(blank=True)
-    hashtags = models.TextField(blank=True,default="")
+    hashtags = models.TextField(blank=True,default="",help_text="Add hashtags as comma separated values.")
     image = models.ImageField(blank=True, upload_to=upload_to)
     upload = models.FileField(blank=True, upload_to=upload_to)
     is_pinned = models.BooleanField(default=False)
