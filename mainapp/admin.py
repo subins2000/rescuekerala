85a37ebd6000b8d69c2e65bedfff9f35d9fb8276
Shameel Kadannamanna <shameelsadaka@gmail.com>
2019-08-11 16:30:50 +0530

    Added Filters to collection centers

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -253,7 +253,10 @@ class HospitalAdmin(admin.ModelAdmin):
     list_display = ('district', 'name', 'officer', 'designation','landline','mobile','email')
 
 
-
+class CollectionCenterAdmin(admin.ModelAdmin):
+    list_filter = ('district',)
+       search_fields = ['name']
+ 
 
 
 class CsvBulkUploadAdmin(admin.ModelAdmin):
@@ -274,7 +277,7 @@ admin.site.register(DistrictNeed)
 admin.site.register(PrivateRescueCamp)
 admin.site.register(DistrictCollection)
 admin.site.register(DistrictManager)
-admin.site.register(CollectionCenter)
+admin.site.register(CollectionCenter,CollectionCenterAdmin)
 admin.site.register(RescueCamp, RescueCampAdmin)
 admin.site.register(NGO, NGOAdmin)
 admin.site.register(Announcements, AnnouncementAdmin)
