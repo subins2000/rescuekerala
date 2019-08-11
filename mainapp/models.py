f29f2443f2b65ca8bb145df4b1ec73aa169d0efb
jino Jossy <jinoj@qburst.com>
2019-08-11 20:36:04 +0530

    completed dynamic announcement title and description

--- mainapp/models.py
+++ mainapp/models.py
@@ -550,7 +550,6 @@ class Announcements(models.Model):
         verbose_name='Priority',
         default='L')
 
-    title = models.TextField(blank=True)
     description = models.TextField(blank=True)
     hashtags = models.TextField(blank=True,default="",help_text="Add hashtags as comma separated values.")
     image = models.ImageField(blank=True, upload_to=upload_to)
